c64cb3ea12c47509a7d82a80e89fe28dea668cf8
DarkEclipz <larsrotgers@gmail.com>
2020-11-15 21:38:42 +0100

    updated normal example

--- Program.cs
+++ Program.cs
@@ -12,11 +12,11 @@ namespace Light2D
         {
             var stopwatch = new Stopwatch();
             var camera = new Camera(500, 500);
-            Scene scene = new ReflectionScene();
+            Scene scene = new HelloWorldScene();
             Sampler sampler = new StratifiedSampler(pixelSamples: 4, lightSamples: 64);
             var renderer = new Renderer(sampler);
             stopwatch.Start();
-            renderer.Render(camera, scene, "images/reflection-off-example.png");
+            renderer.Render(camera, scene, "images/test.png");
             stopwatch.Stop();
             Console.WriteLine($"Total render time: {stopwatch.Elapsed}");
         }
