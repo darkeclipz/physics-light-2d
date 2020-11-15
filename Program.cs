a3b8846dd51455fa2a092c9d5ef0f25d44b2714a
darkeclipz <larsrotgers@gmail.com>
2020-11-15 09:12:20 +0100

    readme update

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ namespace Light2D
         static void Main(string[] args)
         {
             var stopwatch = new Stopwatch();
-            var camera = new Camera(800, 800);
-            Scene scene = new RGBScene();
+            var camera = new Camera(500, 500);
+            Scene scene = new MandelbrotScene();
             Sampler sampler = new StratifiedSampler(pixelSamples: 4, lightSamples: 32);
             var renderer = new Renderer(sampler);
             stopwatch.Start();
-            renderer.Render(camera, scene, "images/rgb-scene.png");
+            renderer.Render(camera, scene, "images/mandelbrot.png");
             stopwatch.Stop();
             Console.WriteLine($"Total render time: {stopwatch.Elapsed}");
         }
